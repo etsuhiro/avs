158bc03755a1a5f76b28de5ed72864a263912d8f
etsuhiro <etsuhiri.wada@nifty.com>
2015-02-20 20:43:25 +0900

    リソーステスト登録

--- ResTest/Resource.h
+++ ResTest/Resource.h
@@ -5,9 +5,8 @@
 #define IDD_ABOUTBOX                            103
 #define IDM_ABOUT                               104
 #define IDM_EXIT                                105
-#define IDI_DX11EXERCISE                        107
+#define IDI_RESTEST                             107
 #define IDI_SMALL                               108
-#define IDC_DX11EXERCISE                        109
-#define IDR_IDC_SPRITE1                         111
-#define IDD_DIALOG1                             112
+#define IDC_RESTEST                             109
+#define IDD_DIALOG1                             110
 #define IDS_APP_TITLE                           40000
